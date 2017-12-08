9052bca55303d54d5e6835d253a1b66f22d215c0
Mohamad <lemirmohammad.shehab@lau.edu>
2017-12-08 16:24:54 +0200

    Initial commit

---
+++ app/src/main/java/mchehab/com/gridviewviewpager/ImageActivity.kt
@@ -0,0 +1,40 @@
+package mchehab.com.gridviewviewpager
+
+import kotlinx.android.synthetic.main.activity_image.*
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageActivity : AppCompatActivity() {
+
+    var position = 0;
+    var listImageURLs = arrayListOf<String>()
+    val listFragments = arrayListOf<ImageFragment>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image)
+
+        getArguments()
+        initFragments()
+
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager, listFragments)
+        viewPager.adapter = viewPagerAdapter
+        viewPager.currentItem = position
+    }
+
+    private fun getArguments(){
+        val bundle = intent.extras
+        position = bundle.getInt("position")
+        listImageURLs = bundle.getStringArrayList("imageURLs")
+    }
+
+    private fun initFragments(){
+        for (i in 0..listImageURLs.size-1){
+            val bundle = Bundle()
+            val imageFragment = ImageFragment()
+            bundle.putString("imageURL", listImageURLs[i])
+            imageFragment.arguments = bundle
+            listFragments.add(imageFragment)
+        }
+    }
+}
